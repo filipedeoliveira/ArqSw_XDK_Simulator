69df071a931b21ec25023f0ef574cea700cdbc39
Stéphane Fernandes <stephanefernandes@hotmail.com>
2016-10-02 11:34:04 +0100

    3

--- XDK_Simulator/src/xdk_simulator/Servidor.java
+++ XDK_Simulator/src/xdk_simulator/Servidor.java
@@ -5,18 +5,70 @@
  */
 package xdk_simulator;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
  *
- * 
+ * @author Stéphane
  */
+
 public class Servidor {
     
+    public static void main(String[] args) throws IOException {
+     // inicia o servidor
+     new Servidor(12345).executa();
+   }
+   
+   private int porta;
+   private List<PrintStream> clientes;
+   
+   public Servidor (int porta) {
+     this.porta = porta;
+     this.clientes = new ArrayList<PrintStream>();
+   }
+   
+   public void executa () throws IOException {
+     ServerSocket servidor = new ServerSocket(this.porta);
+     System.out.println("Porta 12345 aberta!");
+     
+     while (true) {
+       // aceita um cliente
+       Socket cliente = servidor.accept();
+       System.out.println("Nova conexão com o cliente " +   
+         cliente.getInetAddress().getHostAddress()
+       );
+       
+       // adiciona saida do cliente à lista
+       PrintStream ps = new PrintStream(cliente.getOutputStream());
+       this.clientes.add(ps);
+       
+       
+       // cria tratador de cliente numa nova thread
+       TrataCliente tc = 
+           new TrataCliente(cliente.getInputStream(), this);
+            new Thread(tc).start();
+            
+          
+     }
+ 
+   }
+   
+   
+    public void distribuiMensagem(String msg) {
+     // envia msg para todo mundo
+     for (PrintStream cliente : this.clientes) {
+       cliente.println(msg);
+     }
+    }
     
 
-    public static void main(String[] args) throws InterruptedException {
-        
+    public static void FuncAux(){
     int min= 1;
     int max=8;
     float result;
@@ -51,12 +103,13 @@ public class Servidor {
                     System.out.println("A intensidade de Luz é: %.2f " + result);
                      break;
         }
+    try {
         Thread.sleep(5000);
+    } catch (InterruptedException ex) {
+         System.out.println("ERRO");
     }
-     
-
-        
-
     }
-
+}
+ 
+    
 }
---
+++ XDK_Simulator/src/xdk_simulator/TrataCliente.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xdk_simulator;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Stéphane
+ */
+ public class TrataCliente implements Runnable{
+ 
+   private InputStream cliente;
+   private Servidor servidor;
+ 
+   public TrataCliente(InputStream cliente, Servidor servidor) {
+     this.cliente = cliente;
+     this.servidor = servidor;
+   }
+ 
+   public void run() {
+     // quando chegar uma msg, distribui pra todos
+     Scanner s = new Scanner(this.cliente);
+     while (s.hasNextLine()) {
+       servidor.distribuiMensagem(s.nextLine());
+     }
+     s.close();
+   }
+}
